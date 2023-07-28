804f03c42533a59fdf97e498e34c4b4351c8e2b6
Liushiyu-0709 <593608244@qq.com>
2023-07-28 10:21:45 +0800

    first commit

--- demo_quick_start/pyteste_QAexcel.py
+++ demo_quick_start/pyteste_QAexcel.py
@@ -1,4 +1,5 @@
-from dotenv import load_dotenv, find_dotenv
+# from dotenv import load_dotenv, find_dotenv
+import os
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import CSVLoader
@@ -6,7 +7,7 @@ from langchain.vectorstores import DocArrayInMemorySearch
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-
+os.environ["OPENAI_API_KEY"] = "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"
 #env 文件写法 export OPENAI_API_KEY='key'
 def load_csv(file_path):
     loader = CSVLoader(file_path=file_path)
@@ -14,7 +15,17 @@ def load_csv(file_path):
     return docs
 
 def create_embeddings(docs):
-    embeddings = OpenAIEmbeddings()
+    from langchain.embeddings import HuggingFaceEmbeddings
+
+    model_name = 'moka-ai/m3e-base'
+    model_kwargs = {'device': 'cpu'}
+    encode_kwargs = {'normalize_embeddings': False}
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+    #embeddings = OpenAIEmbeddings()
     db = DocArrayInMemorySearch.from_documents(
         docs, 
         embeddings
@@ -28,10 +39,12 @@ def run_query(llm, retriever, query):
     return response
 
 def main():
-    _ = load_dotenv(find_dotenv())
-    file_path = 'OutdoorClothingCatalog_1000.csv'
+    #_ = load_dotenv(find_dotenv())
+    file_path = 'OutdoorClothingCatalog_5.csv'
     docs = load_csv(file_path)
+    print("have loaded")
     retriever = create_embeddings(docs)
+    print("get retriever")
     llm = ChatOpenAI(temperature=0.0, max_tokens=1024)
     query ="Please list all your shirts with sun protection \
     in a table in markdown and summarize each one."
