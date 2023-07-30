91e3bbfc5650bce0a094d5dd6bbbe2c7ea825d17
Angry_Cookie <fanby2014@126.com>
2023-07-30 10:49:03 +0800

    Clear Git cache

--- demo_quick_start/init.py
+++
@@ -1,4 +0,0 @@
-import os
-def init():
-    os.environ["SERPAPI_API_KEY"] = 'a89ebd9538b3c69783d230411f18f0f315c58531c296dda01a2c0d14cd0bc895'
-    os.environ["OPENAI_API_KEY"] = "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"
